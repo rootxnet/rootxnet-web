e98c6ba02dbd3f11a9ea3921492f96ddbb6d7b70
Michał Lech <rootx@rootxnet.com>
2015-10-01 00:45:39 +0200

    Expose wsgi application

--- web/nano.py
+++ web/nano.py
@@ -1,3 +1,4 @@
+from django.core.wsgi import get_wsgi_application
 import os
 import sys
 
@@ -23,5 +24,6 @@ settings.configure(
 
 if __name__ == "__main__":
     from django.core.management import execute_from_command_line
-
     execute_from_command_line(sys.argv)
+else:
+    application = get_wsgi_application()
