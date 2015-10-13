6c509180b83962c13ecd6e36db8017d0fb906731
Michał Lech <rootx@rootxnet.com>
2015-10-13 03:11:10 +0200

    Add IPYNB metadata extractor and handler

--- web/nanodj/context_processors.py
+++ web/nanodj/context_processors.py
@@ -1,3 +1,4 @@
+import json
 import os
 from django.core.urlresolvers import reverse
 from django.conf import settings
@@ -7,10 +8,21 @@ from .views import page
 def all_ipynb_pages(request):
     pages = []
     for file_name, ext in map(os.path.splitext, os.listdir(settings.PAGE_BUILD_DIR)):
-        pages.append({
-            "title": file_name,
-            "url": reverse(page, kwargs={"slug": file_name}),
-        })
+        if ext == ".html":
+            meta_file = os.path.join(settings.PAGE_BUILD_DIR, "{}.METADATA".format(file_name))
+            metadata = None
+            if os.path.isfile(meta_file):
+                with open(meta_file) as meta:
+                    try:
+                        metadata = json.loads(meta.read())
+                    except ValueError:
+                        pass
+
+            pages.append({
+                "title": file_name,
+                "url": reverse(page, kwargs={"slug": file_name}),
+                "metadata": metadata,
+            })
     return {
         "all_ipynb_pages": pages,
     }
--- web/nanodj/management/commands/build_ipynb.py
+++ web/nanodj/management/commands/build_ipynb.py
@@ -1,3 +1,5 @@
+import json
+import io
 import os
 import subprocess
 from django.core.management.base import BaseCommand
@@ -11,7 +13,15 @@ class Command(BaseCommand):
 
         for ipynb_file in os.listdir(settings.PAGE_SOURCE_DIR):
             full_path = os.path.join(settings.PAGE_SOURCE_DIR, ipynb_file)
-            print full_path
+
+            source_filename, _ = os.path.splitext(os.path.basename(full_path))
+            output_meta_path = os.path.join(settings.PAGE_BUILD_DIR, "{}.METADATA".format(source_filename))
+
+            with open(full_path) as source:
+                metadata = json.loads(source.read()).get("metadata")
+                with io.open(output_meta_path, 'w', encoding='utf-8') as out:
+                    out.write(unicode(json.dumps(metadata, ensure_ascii=False)))
+
             popen = subprocess.Popen(
                 args=["nbconvert", "--to=html", "--template=basic", full_path],
                 cwd=settings.PAGE_BUILD_DIR,
