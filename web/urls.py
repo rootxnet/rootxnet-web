4d0d2d0037392045ff94352b718cf67e31467af7
Michal Lech <rootx@rootxnet.com>
2015-10-13 07:12:16 +0000

    Remove trailing slash

--- web/urls.py
+++ web/urls.py
@@ -5,7 +5,7 @@ import nanodj.urls
 urlpatterns = [
     url(r'^blog/$', TemplateView.as_view(template_name='blog.html'), name='blog'),
     url(r'^blog/', include(nanodj.urls), ),
-    url(r'^Michal-Lech-Resume.pdf/$',
+    url(r'^Michal-Lech-Resume.pdf$',
         RedirectView.as_view(
             url="https://www.dropbox.com/s/grkahe90fkbkv1n/Michal-Lech-Resume.pdf?dl=0",
             permanent=False
