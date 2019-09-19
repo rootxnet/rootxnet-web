46b2de272aadbcb5c33d7f0f13270f5f5a351290
Michał Lech <rootx@rootxnet.com>
2019-09-19 05:35:27 +0200

    Output gunicorn logs to stdout

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,12 +7,6 @@ if [[ $RUN_TESTS ]]; then
     cd $APP_HOMEDIR
     tox
 else
-    # Prepare log files and start outputting logs to stdout
-    mkdir $APP_HOMEDIR/logs/
-    touch $APP_HOMEDIR/logs/gunicorn.log
-    touch $APP_HOMEDIR/logs/access.log
-    tail -n 0 -f $APP_HOMEDIR/logs/*.log &
-
     # Start Gunicorn processes
     echo Starting Gunicorn.
     cd $APP_HOMEDIR/web
@@ -21,7 +15,7 @@ else
         --bind 0.0.0.0:8000 \
         --workers 3 \
         --log-level=info \
-        --log-file=$APP_HOMEDIR/logs/gunicorn.log \
-        --access-logfile=$APP_HOMEDIR/logs/access.log \
+        --log-file=- \
+        --access-logfile=- \
         "$@"
 fi
