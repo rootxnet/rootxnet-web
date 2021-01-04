f91469392ed4ae7268a08774230c625f512d704b
requires.io <support@requires.io>
2021-01-04 09:20:56 +0100

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     packages=_packages,
     include_package_data=True,
     install_requires=[
-        "Django==3.1.4",
+        "Django==3.1.5",
         "jupyter==1.0.0",
         "gunicorn==20.0.4",
         "whitenoise==5.2.0",
